320c2d32a90f2a374ffe15871bdea306255d85cb
Tom Marble <tmarble@info9.net>
2015-07-19 10:56:03 -0500

    revert to Clojure 1.7.0-RC1

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
 
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
                  [org.clojure/clojurescript "0.0-3308"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [aleph "0.4.0"]
